06f1e8b2139412273fe8e895ceb7542e39bfbb6f
Ondřej Zeman <ondrejzeman23@gmail.com>
2024-11-22 14:44:04 +0100

    Add Background parallax effect to Masthead

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
 import Image from 'next/image';
 import Masthead from '@/components/masthead';
 import AboutUs from '@/components/aboutus';
-import styles from './Home.module.css';
 import Head from 'next/head';
 
 export default function Home() {
 	return (
-		<div className={styles.container}>
+		<div>
 			<Head>
 				<title>PixelForge Studios</title>
 			</Head>
