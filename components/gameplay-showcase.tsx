1c6f261ba01e300ba8a0e6cc2cd7ac54a3367bea
Ondřej Zeman <ondrejzeman23@gmail.com>
2024-11-23 10:11:50 +0100

    Add pictures to game showcase

--- components/gameplay-showcase.tsx
+++ components/gameplay-showcase.tsx
@@ -5,12 +5,14 @@ const GameplayShowcase = () => {
 	return (
 		<section className='flex flex-col bg-gray-100 py-20'>
 			<div className='container mx-auto px-11'>
-				<h2 className='text-4xl font-bold text-center mb-10'>Gameplay Showcase</h2>
+				<h2 className='text-4xl font-bold text-center mb-10'>
+					Gameplay Showcase ze hry Throne of Ashes
+				</h2>
 				<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
-					{[1, 2, 3, 4, 5, 6].map((i) => (
+					{[1, 2].map((i) => (
 						<div key={i} className='relative h-64 overflow-hidden rounded-lg shadow-md'>
 							<Image
-								src={`/gameplay/screenshot_${i}.jpg`}
+								src={`/gameplay/screenshot_${i}.webp`}
 								alt={`Gameplay screenshot ${i}`}
 								layout='fill'
 								objectFit='cover'
